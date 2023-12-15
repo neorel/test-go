4df41658d9cef0d42c51de2bded1b3acedfa56a8
Aurélien LEMAITRE <chevlu@gmail.com>
2023-12-15 15:33:48 +0100

    feat: fisrt api download

--- go/main.go
+++ go/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/neorel/test-go/model"
+)
 
 func main() {
-	fmt.Println("test");
+	res, err := http.Get("https://ergast.com/api/f1/2011/5/laps/1.json")
+	if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+	fmt.Println(string(data));
+
+	var response model.ErgastResponse
+	json.Unmarshal(data, &response)
+
+	fmt.Println(response);
 }
---
+++ go/model/ergast.go
@@ -0,0 +1,44 @@
+package model
+
+type ErgastResponse struct {
+	MRData ErgastMRData `json:"MRData"`
+}
+
+type ErgastMRData struct {
+	RaceTable ErgastRaceTable `json:"RaceTable"`
+}
+
+type ErgastRaceTable struct {
+	Races []ErgastRace `json:"Races"`
+}
+
+type ErgastRace struct {
+	Season string `json:"season"`
+	RaceName string `json:"raceName"`
+	Circuit ErgastCircuit `json:"Circuit"`
+	Laps []ErgastLap `json:"Laps"`
+}
+
+type ErgastCircuit struct {
+	Id string `json:"circuitId"`
+	Name string `json:"circuitName"`
+	Location ErgastLocation `json:"Location"`
+}
+
+type ErgastLocation struct {
+	Lat string `json:"lat"`
+	Lng string `json:"long"`
+	Locality string `json:"locality"`
+	Country string `json:"country"`
+}
+
+type ErgastLap struct {
+	Number string `json:"number"`
+	Timings []ErgastTiming `json:"Timings"`
+}
+
+type ErgastTiming struct {
+	DriverId string `json:"driverId"`
+	Position string `json:"position"`
+	Time string `json:"time"`
+}
