0f419fd3dbfa61eee21b352d44a3e167e4dc6045
Aurélien LEMAITRE <chevlu@gmail.com>
2023-12-19 02:16:59 +0100

    fix: docker

--- go/main.go
+++ go/main.go
@@ -11,15 +11,13 @@ func main() {
 	services.InitRedis()
 
 	http.HandleFunc("/", getRace)
-	fmt.Printf("Starting server for testing HTTP POST...\n")
+	fmt.Printf("Starting server...\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println(err)
 	}
 }
 
 func getRace(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Path)
-
 	season := r.URL.Query().Get("season")
 	raceNumber := r.URL.Query().Get("number")
 
@@ -36,7 +34,6 @@ func getRace(w http.ResponseWriter, r *http.Request) {
 	raceResults := services.GetRaceResults(season, raceNumber)
 	jsonRaceResults, _ := json.Marshal(raceResults)
 	w.Write(jsonRaceResults)
-	fmt.Println(string(jsonRaceResults))
 
 	go services.RunLaps(season, raceNumber, raceResults.NbLaps)
 }
